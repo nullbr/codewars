36d02155643b5211ec6f055693b8cf66b6081a1a
Bruno Leite <bmarianoleite3@gmail.com>
2022-07-18 15:44:30 -0300

    isprime v1

--- prime_numbers/prime_numbers.rb
+++ prime_numbers/prime_numbers.rb
@@ -1,4 +1,27 @@
 # Test if number is prime
 def isPrime(num)
-  # Your code here
+  return false if num <= 1
+
+  prime = true
+  for div in 2..num - 1
+    if (num % div).zero? && num != div
+      puts div
+      prime = false
+      break
+    elsif div > 1000
+      break
+    end
+  end
+  prime
 end
+
+# puts isPrime(4)
+# puts isPrime(100)
+# puts isPrime(999)
+puts isPrime(958297)
+# puts isPrime(-7)
+# puts isPrime(2)
+# puts isPrime(3)
+# puts isPrime(5)
+# puts isPrime(457)
+# puts isPrime(39229)
