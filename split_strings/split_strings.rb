d497ffd9c1aac210f01d13f37e8d52f6c12969eb
Bruno Leite <bmarianoleite3@gmail.com>
2022-07-24 20:57:49 -0300

    splitting strings

--- split_strings/split_strings.rb
+++ split_strings/split_strings.rb
@@ -1,10 +1,15 @@
 def solution(str)
-    str = str.split('')
-    result = []
-    return 
-    while str.size > 0
-        result << ''
-        result[-1] += str.delete_at(0)
-        result[-1] += str.delete_at(0) || '_'
-    end
+  str = str.split('')
+  result = []
+  
+  while str.size > 0
+    result << ''
+    result[-1] += str.delete_at(0)
+    result[-1] += str.delete_at(0) || '_'
+  end
+
+  result
 end
+
+p solution('abc')
+p solution('abcdef')
